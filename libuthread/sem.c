bff856514f07a25dd957a2b281a03d37b30aa325
zackmcd <zackmcd@ad3.ucdavis.edu@pc21.cs.ucdavis.edu>
2019-11-01 13:28:10 -0700

    start of sem

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -6,17 +6,25 @@
 #include "thread.h"
 
 struct semaphore {
-	/* TODO Phase 1 */
+  size_t count;
+  queue_t blocked_threads;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 1 */
+  sem_t sem = (sem_t)malloc(sizeof(sem_t));
+  if (!sem)
+    return NULL;
+  sem->count = count;
+  sem->blocked_threads = queue_create();
 }
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 1 */
+  if (!sem)
+    return -1;
+  free(sem);
+  return 0;
 }
 
 int sem_down(sem_t sem)
