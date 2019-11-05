567a03371ea094847c4602f5f6d6d6e7a3f899af
zackmcd <zackmcd@ad3.ucdavis.edu@pc21.cs.ucdavis.edu>
2019-11-04 18:00:51 -0800

    tps

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,41 +12,35 @@
 #include "thread.h"
 #include "tps.h"
 
-/* TODO: Phase 2 */
+
 
 int tps_init(int segv)
 {
-	/* TODO: Phase 2 */
   return 0;
 }
 
 int tps_create(void)
 {
   return 0;
-	/* TODO: Phase 2 */
 }
 
 int tps_destroy(void)
 {
   return 0;
-	/* TODO: Phase 2 */
 }
 
 int tps_read(size_t offset, size_t length, char *buffer)
 {
   return 0;
-	/* TODO: Phase 2 */
 }
 
 int tps_write(size_t offset, size_t length, char *buffer)
 {
   return 0;
-	/* TODO: Phase 2 */
 }
 
 int tps_clone(pthread_t tid)
 {
   return 0;
-	/* TODO: Phase 2 */
 }
 
