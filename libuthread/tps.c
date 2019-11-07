74538c42508935cac980079040a9b128368fafa5
zackmcd <zackmcd@ad3.ucdavis.edu@pc21.cs.ucdavis.edu>
2019-11-06 20:57:35 -0800

    starting tps

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,15 +12,64 @@
 #include "thread.h"
 #include "tps.h"
 
+typedef struct page
+{
+  int count;
+  void *mem_add;
+}page, *page_t;
 
+typedef struct tps
+{
+  pthread_t thread;
+  page_t page;
+}tps, *tps_t;
+
+queue_t tps_threads;
+
+static int is_tid(void *data, void *arg)
+{
+  tps_t tps = (tps_t)data;
+  pthread_t tid = *(pthread_t*)arg;
+  if (tps->thread == tid)
+    return 1;
+  else
+    return 0;
+}
 
 int tps_init(int segv)
 {
+  if (tps_threads)
+    return -1;
+
+  tps_threads = queue_create();
+  //signal handling
   return 0;
 }
 
 int tps_create(void)
 {
+  if (!tps_threads)
+    tps_init();
+
+  tps_t check = NULL;
+  pthread_t tid = pthread_self();
+  queue_iterate(tps_threads, is_tid, (void*)(&tid), (void**)&check);
+  if (check)
+    return -1;
+
+  tps_t t = (tps_t)malloc(sizeof(tps));
+  if (!t)
+    return -1;
+  t->thread = tid;
+  
+  page_t p = (page_t)malloc(sizeof(page));
+  if (!p)
+    return -1;
+  t->page = p;
+  t->page->count = 1;
+  t->page->mem_add = //mmap;
+  queue_enqueue(tps_threads, (void*)t);
+
   return 0;
 }
 
@@ -31,6 +80,7 @@ int tps_destroy(void)
 
 int tps_read(size_t offset, size_t length, char *buffer)
 {
+  
   return 0;
 }
 
